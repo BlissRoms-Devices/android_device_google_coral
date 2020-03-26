9f39d38f50d9c6016e1208beb2c43afed0b18f65
wrongway213 <tropeazy@gmail.com>
2020-03-26 16:50:48 -0400

    Build with Unicornblood

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -54,7 +54,7 @@ BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/google/floral
-TARGET_KERNEL_CONFIG := fsociety_defconfig
+TARGET_KERNEL_CONFIG := floral_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
